18ac2bf8c98b683e2089c0d479e03cf1e20be6e4
gomezclaudio11 <claudiogomez23@gmail.com>
2025-01-31 10:28:50 -0300

    Expresiones Regulares

---
+++ pythonMoure/07_regular_expressions.py
@@ -0,0 +1,55 @@
+### >Expresiones regulares
+
+import re   
+
+###match
+my_string = "Esta es la leccion numero 7: Leccion llamada Expresiones Regulares"
+my_other_string = "Esta no es la leccion numero 6: Manejo de ficheros"
+
+print(re.match("Esta es la leccion", my_string))
+print(re.match("numero 7", my_string))# none xq empieza a buscar desde el principio
+
+match = re.match("Esta es la leccion", my_string, re.I)#re.I es un ignore case ignora mayusculas y minusculas
+print(match) #es una tupla
+start, end = match.span()
+print(my_string[start:end] )
+
+match = re.match("Esta no es la leccion", my_other_string)
+if match != None:
+    print(match)
+    start, end = match.span()
+    print(my_other_string[start:end])
+
+###Search
+#solo muestra la primer coincidencia
+search = re.search("leccion", my_string, re.I)
+print(search)
+start, end = search.span()
+print(my_string[start:end])
+
+### findall
+#muestra todas las coincidencias
+findall = re.findall("leccion", my_string, re.I)
+print(findall)
+
+#split
+print(re.split(":", my_string))
+
+#sub
+print(re.sub("Expresiones", "expresiones", my_string))
+
+###Patterns
+
+pattern = r"[a-z]"
+print(re.findall(pattern, my_string))
+
+pattern = r"[0-9]"
+print(re.findall(pattern, my_string))
+print(re.search(pattern, my_string))
+
+pattern = r"\d"
+print(re.findall(pattern, my_string))
+
+
+pattern = r"[l]."
+print(re.findall(pattern, my_string))
