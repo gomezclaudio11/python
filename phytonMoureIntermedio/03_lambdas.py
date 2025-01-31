a5a59565ed48fa56e13b470ed51303c65b4c3186
gomezclaudio11 <claudiogomez23@gmail.com>
2025-01-31 14:16:31 -0300

    elimine copia

--- phytonMoureIntermedio/03_lambdas.py
+++
@@ -1,8 +0,0 @@
-### Lambdas
-### Son funciones anonimas
-
-sum_two_value = lambda first_value, second_value: first_value + second_value
-print(sum_two_value(2, 2))
-
-multiply_value = lambda first, second: first + second - 3
-print(multiply_value(2, 8))
