2fed16fa7f59d2498d40e71d3bb2fc2819bd7175
gomezclaudio11 <claudiogomez23@gmail.com>
2025-02-05 11:45:31 -0300

    Type Hints

---
+++ backEnd/fastAPI/type_hints.py
@@ -0,0 +1,17 @@
+### Type Hints
+
+my_string_variable = "my string variable"
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_string_variable = 5
+print(my_string_variable)
+print(type(my_string_variable))
+
+my_type_variable: str = "my type string variable"
+print(my_type_variable)
+print(type(my_type_variable))
+
+my_type_variable = 5
+print(my_type_variable)
+print(type(my_type_variable))
