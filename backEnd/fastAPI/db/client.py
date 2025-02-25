e0ee535d318dcc2921d010c884c6f00418932bd7
gomezclaudio11 <claudiogomez23@gmail.com>
2025-02-24 23:19:21 -0300

    MongoDB Atlas

--- backEnd/fastAPI/db/client.py
+++ backEnd/fastAPI/db/client.py
@@ -1,4 +1,21 @@
+### MongoDB client ###
+
+# Descarga versión community: https://www.mongodb.com/try/download
+# Instalación:https://www.mongodb.com/docs/manual/tutorial
+# Módulo conexión MongoDB: pip install pymongo
+# Ejecución: sudo mongod --dbpath "/path/a/la/base/de/datos/"
+# Conexión: mongodb://localhost
+
 from pymongo import MongoClient
 
-db_client = MongoClient()
+# Descomentar el db_client local o remoto correspondiente
 
+# Base de datos local MongoDB
+#db_client = MongoClient().local
+
+# Base de datos remota MongoDB Atlas (https://mongodb.com)
+db_client = MongoClient(
+    "mongodb+srv://test:test@cluster0.6hc9b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").test
+# Despliegue API en la nube:
+# Deta - https://www.deta.sh/
+# Intrucciones - https://fastapi.tiangolo.com/deployment/deta/
